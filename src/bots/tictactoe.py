3c5495354c06ac29a231a3587a30b36c2c1a34cf
Nathan <n49874687@gmail.com>
2025-12-21 21:17:37 +0100

    feat: passage en architecture de tournoi

--- src/bots/tictactoe.py
+++ src/bots/tictactoe.py
@@ -2,188 +2,172 @@ import random
 from typing import Optional
 import sys
 import numpy as np
+from collections import defaultdict
 
 from src.learning.neural_network import NeuralNetwork
 from src.games.tictactoe import *
 
-def get_move(board: Board, nn: NeuralNetwork):
-    moves_prob = nn.prediction(board_1D(board))
-
-    legal_moves = board.get_legal_move()
-
-    best_move = None
-    best_score = -float("inf")
-
-    for pos in legal_moves:
-        idx = pos.x * 3 + pos.y
-        score = moves_prob[idx]
-
-        if score > best_score:
-            best_score = score
-            best_move = pos
-
-    return best_move
-
-def get_result(nn1, nn2, display = False):
-    game = Board()
-
-    if random.randint(0, 1) == 0:
-        players = {X: nn1, O: nn2}
-    else:
-        players = {X: nn2, O: nn1}
-
-    while not game.is_ended():
-        current_player = game.get_current_player()
-        nn = players[current_player]
-
-        move = get_move(game, nn)
-
-        try:
-            game[move] = current_player
-        except Exception:
-            return -2 if nn is nn1 else 2
-        
-        if display:
-            print(game, end="\n\n")
-
-    if game.winner is None:
-        return 0
-
-    winner_nn = players[game.winner]
-    return 1 if winner_nn is nn1 else -1
-
-def create_random_nn() -> NeuralNetwork:
-    min_layers = 1
-    max_layers = 3
-
-    n_hidden = random.randint(min_layers, max_layers)
-
-    hidden_sizes = [
-        random.choice([16, 24, 32, 48, 64])
-        for _ in range(n_hidden)
-    ]
-
-    nn = NeuralNetwork(
-        layers_dimensions=[9] + hidden_sizes + [9],
-        hidden_activation_function="relu",
-        output_activation_function="softmax"
-    )
-
-    return nn
-
-def mutate(nn: NeuralNetwork,
-           sigma=0.1) -> NeuralNetwork:
-    """
-    p_weight : proba de muter les poids
-    sigma    : amplitude du bruit
-    """
-
-    weights = [w.copy() for w in nn.weights]
-    biases = [b.copy() for b in nn.biases]
-
-    for i in range(len(weights)):
-        weights[i] += np.random.normal(0, sigma, weights[i].shape)
-        biases[i]  += np.random.normal(0, sigma, biases[i].shape)
-
-    return NeuralNetwork(
-        weights=weights,
-        biases=biases,
-        hidden_activation_function=nn.hidden_activation_function,
-        output_activation_function=nn.output_activation_function
-    )
-
 def board_1D(board, player=O):
     return [1 if cell == player else 0 if cell is BLANK else -1 for row in board for cell in row]
 
-def play(nn: NeuralNetwork):
-    game = Board()
+class Evolution:
+    def __init__(self, n:int):
+        """
+        Classe Evolution permettant de sélectionner les meilleurs réseaux parmi n réseaux
+        
+        :param n: Nombre de réseaux total 
+        :type n: int
+        """
+        self.n = n
+        self.nns = [self._create_random_nn() for _ in range(n)]
 
-    print("=== Morpion : Humain vs IA ===")
-    print("Tu joues X, l'IA joue O\n")
+    def train(self, epoch:int):
+        """
+        Sélectionne les meilleures IA dans un tournoi puis réitère
+        
+        :param epoch: Nombre d'itérations
+        :type epoch: int
+        """
+        k = int(1/3 * self.n)
 
-    while not game.is_ended():
-        print(game)
+        for e in range(epoch):
+            scores = self.tournament()
+            self.nns.sort(key= lambda x: scores[x], reverse=True)
 
-        if game.get_current_player() == X:
-            while True:
-                try:
-                    raw = input("Entre ton coup (ligne col) [0-2 0-2] : ")
-                    r, c = map(int, raw.split())
-                    pos = Position(r, c)
+            for idx in range(k, int(self.n * 2/3)):
+                self.nns[idx] = self._mutate(self.nns[idx % k])
 
-                    if pos not in game.get_legal_move():
-                        print("Coup illégal, réessaie.")
-                        continue
+            for idx in range(int(self.n * 2/3), self.n):
+                self.nns[idx] = self._create_random_nn()
 
-                    game[pos] = X
-                    break
+            sys.stdout.write(
+                f"\rEpoch : {e}/{epoch} "
+                f"Best score : {scores[self.nns[0]]}"
+            )
 
-                except Exception:
-                    print("Entrée invalide.")
+    def tournament(self, n_matches:int = 10) -> list:
+        """
+        Renvoie les modèles du meilleur au pire par leurs performance dans un tournoi
+        
+        :param n_matches: Nombre de matchs joués par chaque modèles
+        :type n_matches: int 
+        :return: Liste des réseau triés du plus performant au pire
+        :rtype: list
+        """
+        scores = defaultdict(int)
 
-        else:
-            # --- coup IA ---
-            pos = get_move(game, nn)
-            game[pos] = O
-            print(f"IA joue : {pos.x} {pos.y}")
+        for m in self.nns:
+            opponents = random.sample(self.nns, n_matches)
+            for o in opponents:
+                if m is o:
+                    continue
+                if random.uniform(0, 1) < 0.5:
+                    s = self.duel(m, o)
+                else:
+                    s = self.get_game_score(m, o)
 
-    print(game)
+                scores[m] += s * (5 if s < 0 else 1)
+                scores[o] -= s * (5 if s > 0 else 1)
+        
+        return scores
 
-    if game.winner == X:
-        print("Tu as gagné !")
-    elif game.winner == O:
-        print("L'IA a gagné.")
-    else:
-        print("Match nul.")
+    def duel(self, nn1, nn2):
+        s1 = self.get_game_score(nn1, nn2)
+        s2 = self.get_game_score(nn2, nn1)
+        return s1 - s2
 
+    def get_game_score(self, nn1, nn2, display = False, early_stop = False):
+        game = Board()
 
-def main():
-    nn = create_random_nn()
-    nn2 = create_random_nn()
+        players = {X: nn1, O: nn2}
+        coups = 0
 
-    wins_1 = 0
-    wins_2 = 0
-    draws = 0
-    games = 0
+        while not game.is_ended():
+            if early_stop:
+                if coups >= 5 and game.winner is None:
+                    return 0
+            
+            current_player = game.get_current_player()
+            nn = players[current_player]
 
-    for epoch in range(10000):
-        #print(epoch)
-        score = get_result(nn, nn2)
+            move = self._get_move(game, nn)
 
-        if score == -2 or score == 2:
-            if score == -2:
-                nn = create_random_nn()
-            else:
-                nn2 = create_random_nn()
-            continue
+            try:
+                game[move] = current_player
+            except Exception:
+                return -0.1 if nn is nn1 else 0.1
+            
+            coups += 1
+            
+            if display:
+                print(game, end="\n\n")
 
-        games += 1
+        if game.winner is None:
+            return 0.2
 
-        if score == 1:
-            wins_1 += 1
-            nn = nn
-            nn2 = mutate(nn)
-        elif score == -1:
-            wins_2 += 1
-            nn = nn2
-            nn2 = mutate(nn)
-        else:
-            draws += 1
-
-        p1 = wins_1 / games * 100
-        p2 = wins_2 / games * 100
-        pd = draws / games * 100
-
-        bar = lambda p: "█" * int(p / 5)
-
-        sys.stdout.write(
-            f"\rGAMES {games}/{epoch} | "
-            f"NN1 [{bar(p1):20}] {p1:5.1f}% | "
-            f"NN2 [{bar(p2):20}] {p2:5.1f}% | "
-            f"Draws [{bar(pd):20}] {pd:5.1f}%"
-        )
-        sys.stdout.flush()
+        winner_nn = players[game.winner]
+        get_score = lambda x: x - coups/10
+        return get_score(1 if winner_nn is nn1 else -1)
     
-    get_result(nn, nn2, True)
-    play(nn)
-    play(nn2)
+    def _create_random_nn(self) -> NeuralNetwork:
+        min_layers = 1
+        max_layers = 3
+
+        n_hidden = random.randint(min_layers, max_layers)
+
+        hidden_sizes = [
+            random.choice([16, 24, 32, 48, 64])
+            for _ in range(n_hidden)
+        ]
+
+        nn = NeuralNetwork(
+            layers_dimensions=[9] + hidden_sizes + [9],
+            hidden_activation_function="relu",
+            output_activation_function="linear"
+        )
+
+        return nn
+
+    def _get_move(self, board: Board, nn: NeuralNetwork):
+        moves_prob = nn.prediction(board_1D(board))
+
+        legal_moves = board.get_legal_move()
+
+        best_move = None
+        best_score = -float("inf")
+
+        for pos in legal_moves:
+            idx = pos.x * 3 + pos.y
+            score = moves_prob[idx]
+
+            if score > best_score:
+                best_score = score
+                best_move = pos
+
+        return best_move
+    
+    def _mutate(self, nn: NeuralNetwork,
+            sigma=0.02) -> NeuralNetwork:
+        """
+        p_weight : proba de muter les poids
+        sigma    : amplitude du bruit
+        """
+
+        weights = [w.copy() for w in nn.weights]
+        biases = [b.copy() for b in nn.biases]
+
+        for i in range(len(weights)):
+            weights[i] += np.random.normal(0, sigma, weights[i].shape)
+            biases[i]  += np.random.normal(0, sigma, biases[i].shape)
+
+        return NeuralNetwork(
+            weights=weights,
+            biases=biases,
+            hidden_activation_function=nn.hidden_activation_function,
+            output_activation_function=nn.output_activation_function
+        )
+
+def main(): 
+    evol = Evolution(75)
+    evol.train(200)
+    evol.get_game_score(evol.nns[0], evol.nns[1], True, False)
